19f67442b23cc99f7b04b86049b50a96f90cf0b4
AnuragTomar <AnuragTomar91@gmail.com>
2017-06-28 16:00:40 +0530

    using local mongodb instance

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,8 +12,7 @@ var catalog = require('./routes/catalog');  //Import routes for "catalog" area o
 
 var app = express();
 //Set up mongoose connection
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb://anuragtomar91:a8237715233@ds135382.mlab.com:35382/local-library';
+var mongoDB = 'mongodb://127.0.0.1/librarydb';
 mongoose.connect(mongoDB);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
