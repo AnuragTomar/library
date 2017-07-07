bd1504ada8366ca923db4043972685fe88dc6692
AnuragTomar <AnuragTomar91@gmail.com>
2017-07-07 17:13:28 +0530

    change book model

--- models/book.js
+++ models/book.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-
+var moment = require('moment');
 var Schema = mongoose.Schema;
 
 var BookSchema = new Schema({
@@ -7,7 +7,10 @@ var BookSchema = new Schema({
   author: {type: Schema.ObjectId, ref: 'Author', required: true},
   summary: {type: String, required: true},
   isbn: {type: String, required: true},
-  genre: [{type: Schema.ObjectId, ref: 'Genre'}]
+  genre: [{type: Schema.ObjectId, ref: 'Genre'}],
+  published_date: {type : Date},
+  total_pages : {type: Number}
+  angie_meter : {type: Number}
 });
 
 // Virtual for book's URL
@@ -17,6 +20,11 @@ BookSchema
   return '/catalog/book/' + this._id;
 });
 
+BookSchema
+.virtual('published_date')
+.get(function () {
+  return moment(this.published_date).format('MMMM Do, YYYY');
+});
 //Export model
 module.exports = mongoose.model('Book', BookSchema);
 
