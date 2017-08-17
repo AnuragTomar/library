f3111f8dbe89a814b9d93282a8498bb4d47966e4
AnuragTomar <AnuragTomar91@gmail.com>
2017-08-17 15:40:47 +0530

    remove pug files in views folder and start adding angular dependencies

--- client/src/app/sign-up.component.ts
+++
@@ -1,6 +0,0 @@
-import { Component }  from '@angular/core';
-
-@Component({
-selector: 'sign-up',
-}) 
-export class SignUpComponent{}
---
+++ e2e/app.po.ts
@@ -0,0 +1,25 @@
+import { browser, by, element } from 'protractor';
+
+export class mynappPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
+
+/*
+import { $$, browser } from 'protractor';
+
+export class Angular2FullStackPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getNavbarElement(n) {
+    return $$('app-root a').get(n).getText();
+  }
+
+}*/
