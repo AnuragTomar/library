f5149460f9448950302602c85ff4362c5b210918
AnuragTomar <AnuragTomar91@gmail.com>
2017-08-30 16:55:33 +0530

    try sub schema

--- server/models/book.ts
+++ server/models/book.ts
@@ -1,11 +1,20 @@
 import * as mongoose from 'mongoose';
 
+const comment = new mongooose.Schema({
+	uname       : {type: mongoose.Schema.ObjectId, ref='User'},
+	fav_part    : String,
+	descriptopn : String,
+	comment_date: {type:Date, default:Date.now}
+});  
+
 const bookSchema = new mongoose.Schema({
-	name	: {type:String, required: true},
-	author  : [{type:mongoose.Schema.ObjectId, ref:'Author', required: true}],
-	genre	: [String],
-	comment : [{type:mongoose.Schema.ObjectId, ref:'Comment'}]
+	name		: {type:String, required: true},
+	discription : String,
+	author  	: [{type:mongoose.Schema.ObjectId, ref:'Author', required: true}],
+	genre		: [String],
+	comments	: [comment]
 });
+
 
 const Book = mongoose.model('Book',bookSchema);
 
